30ba84cde6bbe669cfdd805fc5d69f91ce9da797
Gursahib Singh <gursahib99888@gmail.com>
2023-12-27 06:40:25 -0800

    New Commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let score = 0;
+
+    // Stopwatch code
+    let timer;
+    let seconds = 0;
+    startTimer(); 
+
+
+
     fetch('quizData.json') // Replace 'quizData.json' with the path to your file
         .then(response => {
             if (!response.ok) {
@@ -7,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json();
         })
         .then(quizData => {
+
             const quizContainer = document.getElementById('quiz');
             quizData.forEach((item, index) => {
                 const questionBlock = document.createElement('div');
@@ -22,6 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     li.textContent = option;
                     li.addEventListener('click', function() {
                         handleOptionClick(li, questionBlock, item);
+                        updateScore(li.textContent === item.answer);
+
                     });
                     optionsList.appendChild(li);
                 });
@@ -54,6 +66,13 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
 
+        function updateScore(isCorrect) {
+            if (isCorrect) {
+                score++;
+                document.getElementById('scorecard').textContent = `Score: ${score}`;
+            }
+        }
+
     function resetStyles(questionBlock) {
         questionBlock.querySelectorAll('li').forEach(function(li) {
             li.classList.remove('correct', 'incorrect', 'correct-answer');
@@ -67,4 +86,56 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    function startTimer() {
+        timer = setInterval(function() {
+            seconds++;
+            document.getElementById('stopwatch').textContent = formatTime(seconds);
+        }, 1000);
+    }
+
+    function formatTime(totalSeconds) {
+        let minutes = Math.floor(totalSeconds / 60);
+        let secs = totalSeconds % 60;
+        return `${pad(minutes)}:${pad(secs)}`;
+    }
+
+    function pad(number) {
+        return number < 10 ? '0' + number : number;
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+// Function to submit the quiz or retake the quiz
+function handleSubmitOrRetake() {
+    const submitButton = document.getElementById('submitQuiz');
+    if (submitButton.textContent === "Submit Quiz") {
+        // Stop the timer and show results
+        stopTimer();
+        document.getElementById('timeTaken').textContent += formatTime(seconds);
+        document.getElementById('finalScore').textContent += score;
+        document.getElementById('results').style.display = 'block';
+        // Change the button text to "Retake Quiz"
+        submitButton.textContent = "Retake Quiz";
+
+        // Scroll to the bottom of the page
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth' // Optional: Add smooth scrolling effect
+        });
+    } else {
+        window.scrollTo({
+            top: document.body.scrollTop,
+            behavior: 'smooth' // Optional: Add smooth scrolling effect
+
+        });
+        setTimeout(() => {
+        location.reload();},900);
+    }
+}
+
+document.getElementById('submitQuiz').addEventListener('click', handleSubmitOrRetake);
+
 });
